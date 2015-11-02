228d96bfe48561bd7daeedcdfa95a81e468b6f99
Ole Espen Klem <eklem@Oles-MacBook-Pro.local>
2015-11-02 13:05:00 +0100

    Changes reflecting search-index API changes. Autocomplete, searching and filters working again.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,13 +10,13 @@ searchcatControllers.controller('SearchListCtrl', ['$sce', '$http', '$scope', '$
     var queryString = "";
     $scope.searchresult = Search.srch.query($location.search());
     try {
-      queryString = JSON.parse(queryObject['JSONq'])['query']['*']
+      queryString = JSON.parse(queryObject['q'])['query']['*']
     } catch(e){}
     $scope.searchTerm = queryString;
 
 
     $scope.matcher = function(suggestion) {
-      return $http.get("http://localhost:3030/matcher?beginsWith=" + suggestion)
+      return $http.get('http://localhost:3030/matcher?match={"beginsWith":' + JSON.stringify(suggestion) + '}')
         .then(function (response) {
           return response.data;
         });
@@ -27,15 +27,15 @@ searchcatControllers.controller('SearchListCtrl', ['$sce', '$http', '$scope', '$
     $scope.$watch("searchTerm", function(){ 
       if ($scope.searchTerm) if ($scope.searchTerm.length > 2) {
 //something like
-//http://localhost:3030/search?JSONq={"query":{"*":["ethiopia"]},%20"facets":{"mjtheme":{}}}
-        var JSONq = {};
-        JSONq['query'] = {};
-        JSONq['query']['*'] = $scope.searchTerm.split(' ');
-        JSONq['facets'] = {};
-        JSONq['facets']['type'] = {};
-        JSONq['facets']['user'] = {"limit":10};
-        JSONq['facets']['tags'] = {"limit":10};
-        queryObject['JSONq'] = JSON.stringify(JSONq);
+//http://localhost:3030/search?q={"query":{"*":["ethiopia"]},%20"facets":{"mjtheme":{}}}
+        var q = {};
+        q['query'] = {};
+        q['query']['*'] = $scope.searchTerm.split(' ');
+        q['facets'] = {};
+        q['facets']['type'] = {};
+        q['facets']['user'] = {"limit":10};
+        q['facets']['tags'] = {"limit":10};
+        queryObject['q'] = JSON.stringify(q);
         $scope.searchresult = Search.srch.query(queryObject);
       }
     })
@@ -47,7 +47,7 @@ searchcatControllers.controller('SearchListCtrl', ['$sce', '$http', '$scope', '$
       if (!newQuery.filter) newQuery.filter = {};
       if (!newQuery.filter[facetGroup.key]) newQuery.filter[facetGroup.key] = [];
       newQuery.filter[facetGroup.key].push([facetEntry.gte, facetEntry.lte]);
-      var url = '/app/#/search?JSONq=' + JSON.stringify(newQuery);
+      var url = '/app/#/search?q=' + JSON.stringify(newQuery);
       return url;
     };
 
@@ -65,7 +65,7 @@ searchcatControllers.controller('SearchListCtrl', ['$sce', '$http', '$scope', '$
       if ((newQuery.filter[facetGroup.key]).length == 0) delete newQuery.filter[facetGroup.key];
       if (Object.keys(newQuery.filter).length == 0) delete newQuery.filter;
       //
-      var url = '/app/#/search?JSONq=' + JSON.stringify(newQuery);
+      var url = '/app/#/search?q=' + JSON.stringify(newQuery);
       return url;
     };
 
